df0b4c411d9503282c81406df923aa234132963d
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-01-16 12:08:36 +0300

    The first commit to our php classes

---
+++ conditions.php
@@ -0,0 +1,25 @@
+<?php
+
+//$result = 1 < 3;
+$firstName = 'Jeremy';
+$lastName = 'McPeak';
+
+//the 'and' operator
+if ($firstName == 'jeremy' || $lastName == 'Mcpeak') {
+    echo 'The first condtion is true';
+} else if ($firstName == 'Jeremy' && $lastName == 'McPeak') {
+    echo 'The socond condition is true';
+} else {
+    echo 'The condition is false';
+}
+
+/*$first = 'Jeremy';
+$last = 'McPeak';
+//the 'or' operator
+if ($first == 'emy' || $last == 'McPeak') {
+    echo 'The condtion is true';
+} else {
+    echo 'The condition is false';
+};*/
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>This is the title</title>
+</head>
+<body>
+  <h1>Welcome users</h1>  
+  <?php
+
+  $name = 'Jeremy';
+  $sir = 'Kinsley';
+  
+  echo 'Hello, ' . $name;
+  echo "Hello Mr.$sir";
+  
+  ?>
+</body>
+</html>
