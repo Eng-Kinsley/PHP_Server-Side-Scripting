03cda46b9726e501a250c927215705876ac5e8f3
Eng-Kinsley <119452917+Eng-Kinsley@users.noreply.github.com>
2023-01-16 12:20:42 +0300

    First Commit On Our PHP Classes, enjoy.

---
+++ error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>This is the title</title>
+</head>
+<body>
+  <h1>Welcome users</h1>  
+  <?php
+
+  $name = 'Jeremy';
+  $first_name = "Kinsley";
+  
+  echo "Hello, $name";
+  echo $first_name;
+  
+  ?>
+</body>
+</html>
